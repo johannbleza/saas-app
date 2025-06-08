0d10cc1b11ba17434d7479a19191b3b2773365e4
Johann Sealtiel Bleza <johann.bleza@icloud.com>
2025-06-08 11:24:11 +0800

    implement database setup, create companion server action

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,5 @@
 const CompanionsLibrary = () => {
-    return (
-        <div>
-            CompanionsLibrary
-            
-        </div>
-    );
+  return <div>CompanionsLibrary</div>;
 };
 
 export default CompanionsLibrary;
